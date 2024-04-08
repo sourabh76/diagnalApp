4d1f972e71c094040be6acc7fcf6e5c41470f511
Sourabh76 <sourabhingale76@gmail.com>
2024-04-08 19:20:13 +0530

    Updates

--- src/constants/index.jsx
+++
@@ -1,3 +0,0 @@
-export const BASE_API = "https://test.create.diagnal.com";
-
-export const SET_PAGE = "SET_PAGE";
--- src/redux/Actions/pageActions.js
+++
@@ -1,6 +0,0 @@
-import { SET_PAGE } from "../../constants";
-
-export const setPage = (pageNumber) => ({
-  type: SET_PAGE,
-  payload: pageNumber
-});
